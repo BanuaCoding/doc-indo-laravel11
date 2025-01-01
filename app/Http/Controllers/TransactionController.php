4893ab5d5533adc951eeea75dcc9154354c125cf
adiarhmn <adiauliarahmn@gmail.com>
2025-01-01 10:46:14 +0800

    Update API Create Token

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -3,10 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Midtrans\Config;
+use Midtrans\Snap;
 
 class TransactionController extends Controller
 {
-    public function createTransaction(Request $request){
-        
+    public function createTransaction()
+    {
+        Config::$serverKey = config('app.midtrans.serverKey');
+        Config::$isProduction = false;
+        Config::$isSanitized = true;
+        Config::$is3ds = true;
+
+
+        $params = array(
+            'transaction_details' => array(
+                'order_id' => rand(),
+                'gross_amount' => 10000,
+            ),
+            'customer_details' => array(
+                'first_name' => 'budi',
+                'last_name' => 'pratama',
+                'email' => 'budi.pra@example.com',
+                'phone' => '08111222333',
+            ),
+        );
+
+        $snapToken = Snap::getSnapToken($params);
+
+        return response()->json([
+            'error' => false,
+            'message' => 'Success',
+            'data' => $snapToken
+        ]);
     }
 }
